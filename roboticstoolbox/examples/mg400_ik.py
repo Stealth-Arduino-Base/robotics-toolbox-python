41989b41272205ebd0d62d41c7aa708bc022f2b2
cuiguangzhe123 <51836629+cuiguangzhe123@users.noreply.github.com>
2023-02-10 06:12:33 -0600

    testing ik

---
+++ roboticstoolbox/examples/mg400_ik.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""
+@author John Skinner
+"""
+
+import swift
+import roboticstoolbox as rp
+import numpy as np
+
+
+# Create a puma in the default zero pose
+puma = rp.models.MG400()
+T = puma.fkine(puma.qr)
+print(T)
+sol = puma.ikine_LM(T)
+print("residual = ", np.linalg.norm(T - puma.fkine(sol.q)))
+
+
+# puma.q = puma.qz
+# env.add(puma, robot_alpha=True, collision_alpha=False)
+
+# dt = 0.05
+# interp_time = 5
+# wait_time = 2
+
+# poses = [puma.qr, puma.qz]
+
+# # Pass through the reference poses one by one.
+# # This ignores the robot collisions, and may pass through itself
+# for previous, target in zip(poses[:-1], poses[1:]):
+#     for alpha in np.linspace(0.0, 1.0, int(interp_time / dt)):
+#         puma.q = previous + alpha * (target - previous)
+#         env.step(dt)
+#     for _ in range(int(wait_time / dt)):
+#         puma.q = target
+#         env.step(dt)
+
+# # Uncomment to stop the browser tab from closing
+# env.hold()
