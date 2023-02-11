5956a3de071600c0908373073130095cb66c8395
cuiguangzhe123 <51836629+cuiguangzhe123@users.noreply.github.com>
2023-02-11 04:14:15 -0600

    gripper attached, readjust joint

--- roboticstoolbox/examples/mg400.py
+++ roboticstoolbox/examples/mg400.py
@@ -10,25 +10,26 @@ import numpy as np
 env = swift.Swift()
 env.launch(realtime=True)
 
-# Create a puma in the default zero pose
-puma = rp.models.MG400()
-puma.q = puma.qz
-env.add(puma, robot_alpha=True, collision_alpha=False)
+# Create a mg400 in the default zero pose
+mg400 = rp.models.MG400()
+print(mg400)
+mg400.q = mg400.qz
+env.add(mg400, robot_alpha=True, collision_alpha=False)
 
 dt = 0.05
 interp_time = 5
 wait_time = 2
 
-poses = [ puma.qz,puma.qt]
+poses = [ mg400.qz,mg400.qt]
 
 # Pass through the reference poses one by one.
 # This ignores the robot collisions, and may pass through itself
 for previous, target in zip(poses[:-1], poses[1:]):
     for alpha in np.linspace(0.0, 1.0, int(interp_time / dt)):
-        puma.q = previous + alpha * (target - previous)
+        mg400.q = previous + alpha * (target - previous)
         env.step(dt)
     for _ in range(int(wait_time / dt)):
-        puma.q = target
+        mg400.q = target
         env.step(dt)
 
 # Uncomment to stop the browser tab from closing
--- roboticstoolbox/models/URDF/MG400.py
+++ roboticstoolbox/models/URDF/MG400.py
@@ -42,7 +42,7 @@ class MG400(ERobot):
             links,
             name=name,
             manufacturer="Dobot",            
-            gripper_links=links[4],
+            gripper_links=links[6],
             urdf_string=urdf_string,
             urdf_filepath=urdf_filepath,
         )
@@ -55,15 +55,14 @@ class MG400(ERobot):
         j3 = 1
         
         #[j1,j2,j3,   j3,j3,j3,j2,j3,j3]
-        self.qr = np.array([j1,j2,j3,j3,-j2-j3,j3,j2,-j3,j3+j3])
-        self.qz = np.zeros(7)
+        self.qz = np.zeros(8)
 
         j1= 0
         j2 = 0.3
-        j3 = 0.4
-        self.qt = np.array([j1,j2,j3,0,-j2-j3,j3,j2,-j3,j3+j3])
+        j3 = 1
+        # self.qt = np.array([j1,j2,j3,j3,-j2-j3,j3,j2,-j3])
+        self.qt = np.array([j1,j2,-j2,-j2+j3,-j3,j2,-j2,j3])
 
-        self.addconfiguration("qr", self.qr)
         self.addconfiguration("qz", self.qz)
         self.addconfiguration("qt", self.qt)
 
