9215750b5998754549a84a88b29701659dc03d72
cuiguangzhe123 <51836629+cuiguangzhe123@users.noreply.github.com>
2023-02-12 01:19:20 -0600

    Update mg400_ik.py

--- roboticstoolbox/examples/mg400_ik.py
+++ roboticstoolbox/examples/mg400_ik.py
@@ -26,22 +26,27 @@ l_frame.attach_to(robot.links[-1])
 env.add(l_frame)
 
 
+arrived = False
+dt = 0.05
+
 while True:
     
     Tep = sm.SE3.Rz(90*random.random(), unit='deg')*(robot.fkine(robot.qt)* sm.SE3.Trans(random.random()/10.0-0.02, 0,random.random()/10.0-0.08))
     sol = robot.ikine_LM(Tep)
 
-    arrived = False
-    dt = 0.05
-    l_target = sg.Sphere(0.01, color=[0.2, 0.4, 0.65, 0.5], base=Tep)
+    l_target = sg.Sphere(0.01, color=[0.2, 0.4, 0.65, 0.5], base=robot.fkine(sol.q))
     l_target_frame = sg.Axes(0.1, base=Tep)
+    
+    
     env.add(l_target)
     env.add(l_target_frame)
     robot.q = sol.q
     gain = [1,1,1,1,1,1]
     env.step(dt)
     print("residual = ", np.linalg.norm(Tep - robot.fkine(sol.q)))
+    print("robot joints are: ", [math.degrees(j) for j in robot.q[:-1]])
     time.sleep(3)
+    
 
 # Uncomment to stop the browser tab from closing
 # env.hold()
